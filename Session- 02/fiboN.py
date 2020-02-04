9dcf107da345791fd22268b9939a6e319a5ce156
aosoriom <a.osoriom.2019@alumnos.urjc.es>
2020-02-04 16:34:40 +0100

    Fibon

---
+++ Session- 02/fiboN.py
@@ -0,0 +1,11 @@
+def fibon(n):
+    n1 = 0
+    n2 = 1
+    for i in range (n):
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+    return n1
+print ("5th Fibonacci term", fibon(5))
+print ("10th Fibonacci term", fibon(10))
+print ("15th Fibonacci term", fibon(15))
