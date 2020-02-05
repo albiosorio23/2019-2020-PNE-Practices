5a1708db3307d9b11a8ab019c9f2be9403223a2d
aosoriom <a.osoriom.2019@alumnos.urjc.es>
2020-02-05 18:13:29 +0100

    Create a file

--- Session-03/dna_count_file.py
+++ Session-03/dna_count_file.py
@@ -1,14 +1,20 @@
+sum_A = 0
+sum_C = 0
+sum_T = 0
+sum_G = 0
+total_length = 0
 with open('dna.txt', 'r') as f:
-    header = next(f).replace("\n","")
-    for character in user_input:
-        if character == "A":
-            sum_A = sum_A + 1
-        elif character == "C":
-            sum_C = sum_C + 1
-        elif character == "T":
-            sum_T += 1
-        elif character == "G":
-            sum_G += 1
-        total_length += 1
+    for line in f:
+        for character in line:
+            if character == "A":
+                sum_A = sum_A + 1
+            elif character == "C":
+                sum_C = sum_C + 1
+            elif character == "T":
+                sum_T += 1
+            elif character == "G":
+                sum_G += 1
+            total_length += 1
+    f.close()
 
 print ("The total lenght is:", total_length, "\nA:", sum_A,"\nC:", sum_C, "\nT:", sum_T,"\nG:", sum_G)
