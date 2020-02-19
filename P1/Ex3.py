070fa941aa4a1ab668bc3e570df75ba4cbbf6f4a
aosoriom <a.osoriom.2019@alumnos.urjc.es>
2020-02-19 18:23:03 +0100

    Check if the sequence is null, correct or incorrect

--- P1/Ex3.py
+++ P1/Ex3.py
@@ -7,3 +7,9 @@ s2 = Seq("ACTGA")
 
 # -- Create an invalid sequence
 s3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
+print(f"Sequence 1: {s3}")
+
+
--- P1/Seq1.py
+++ P1/Seq1.py
@@ -3,14 +3,14 @@ class Seq:
     def __init__(self, strbases="NULL"): #espera que le mandes una secuencia, al ponerle el null si está vacía no te da error
         if strbases== "NULL":
             self.strbases = "NULL"
-            print("NULL Seq created")
+            print("NULL Sequence created")
             return
 
         for element in strbases:
             list = ["A", "C", "G", "T"]
             if element not in list:
                 self.strbases = "Error"  # nombre que le das dentro del objeto (self.) ahora queremos comprobar si es correcto antes de almacenarlo
-                print("Error")
+                print("Invalid sequence")
                 return
 
         self.strbases = strbases
