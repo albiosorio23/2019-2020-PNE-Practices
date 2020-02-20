f55080d52f856b4f5c166888c026d70119b6d323
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-02-20 23:36:31 +0100

    Count bases of null, correct and incorrect seq making a dictionary

--- P1/Ex6.py
+++ P1/Ex6.py
@@ -5,9 +5,9 @@ s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
 
 print(f"Sequence 1: (Length: {s1.len()}) {s1}")
-print(s1.count())
+print(f"  Bases: {s1.count()}")
 print(f"Sequence 2: (Length: {s2.len()}) {s2}")
-print(s2.count())
+print(f"  Bases: {s2.count()}")
 print(f"Sequence 3: (Length: {s3.len()}) {s3}")
-print(s3.count())
+print(f"  Bases: {s3.count()}")
 #que se mete dentro del count??
