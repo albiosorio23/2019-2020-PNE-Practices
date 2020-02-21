ca6b072e09ce7f0ac92adc2d274b67fcfcb65c0c
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-02-21 16:13:02 +0100

    Most frequent base of each sequence of genes

---
+++ P1/Ex10.py
@@ -0,0 +1,17 @@
+from Seq1 import Seq
+
+s = Seq()
+FOLDER = "../Session-04/"
+list = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN"]
+bases = ["A","G", "C", "T"]
+
+for element in list:
+    seq = s.read_fasta(FOLDER + element)
+    dict_bases = s.count()
+    minimum = 0 #Para que se vaya quedando con el menor valor de los que vaya leyendo
+    best = ""
+    for base, value in dict_bases.items():
+        while value > minimum:
+            minimum = value
+            best = base
+    print ("Gene", element,": Most frequent Base:", best)
