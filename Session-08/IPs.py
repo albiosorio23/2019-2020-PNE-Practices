a9e99a7d747c4020377869a38cf13f1636a5b2f3
aosoriom <a.osoriom.2019@alumnos.urjc.es>
2020-02-25 19:39:06 +0100

    Table with the IP

---
+++ Session-08/IPs.py
@@ -0,0 +1,4 @@
+Machine       | Interface   | Date        |  Place       |   IP            |
+--------------|-------------|-------------|--------------|-----------------|
+Mobile        | Wifi        | 2020-Feb-24 | Alcorcon Lab | 10.3.33.248     |
+Lab computer  | Wire        |2020-Feb-24  | Alcorcon Lab | 212.128.253.157 |
