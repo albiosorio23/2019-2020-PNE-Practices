9f62ad387c146d7fd213d90f514087e2269ded68
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-03-14 13:20:02 +0100

    Implement the debug_talk() method

--- P2/Client0.py
+++ P2/Client0.py
@@ -2,7 +2,6 @@ import socket
 import termcolor
 
 
-
 class Client:
     def ping(self):
         print("OK!")
@@ -11,4 +10,33 @@ class Client:
         self.ip = ip
         self.port = port
 
+    def __str__(self):
+        return f"Connection to SERVER at {self.ip}, PORT: {self.port}"
 
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+
+        # Send data.
+        s.send(str.encode(msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
+
+    def debug_talk(self, to_server):
+        from_server = self.talk(to_server)
+        print("To Server: ", end="")
+        termcolor.cprint(to_server, "blue")
+        print("From Server: ", end="")
+        termcolor.cprint(from_server, "green")
+
+        return from_server
--- P2/Ex3.py
+++ P2/Ex3.py
@@ -1,19 +1,13 @@
 from Client0 import Client
-
 PRACTICE = 2
-EXERCISE = 1
-
+EXERCISE = 3
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
 IP = "127.0.0.1"
 PORT = 8080
-
-# -- Create a client object
 c = Client(IP, PORT)
 
-# -- Test the ping method
-c.ping()
-
-# -- Print the IP and PORTs
-print(f"IP: {c.ip}, {c.port}")
+print("Sending a message to the server...")
+response = c.talk("Testing!!!")
+print(f"Response: {response}")
