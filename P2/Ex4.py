88169cccf70301f99288f63b326c3632ebe86287
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-03-14 13:19:38 +0100

    Implement the debug_talk() method

---
+++ P2/Ex4.py
@@ -0,0 +1,13 @@
+from Client0 import Client
+PRACTICE = 2
+EXERCISE = 4
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "127.0.0.1"
+PORT = 8080
+c = Client(IP, PORT)
+print (c)
+
+c.debug_talk("Message 1---")
+c.debug_talk("Message 2: Testing !!!")
