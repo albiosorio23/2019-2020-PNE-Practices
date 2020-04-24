1118626914eb96d01e4a1adbe88d47edf2652899
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-04-24 16:23:29 +0200

    Client for testing our server

---
+++ P3/test-client.py
@@ -0,0 +1,33 @@
+from Client0 import Client
+
+PRACTICE = 3
+EXERCISE = 7
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "127.0.0.1"
+PORT = 8080
+
+# -- Cofigure the client
+c = Client(IP, PORT)
+print(c)
+
+print("Connection to SERVER at", IP, ", PORT: ", PORT)
+
+# Test PING
+print("* Testing PING...")
+print(c.talk("PING"))
+
+# Test GET
+print("* Testing GET...")
+print("GET 0:", c.talk("GET 0"))
+print("GET 1:", c.talk("GET 1"))
+print("GET 2:", c.talk("GET 2"))
+print("GET 3:", c.talk("GET 3"))
+print("GET 4:", c.talk("GET 4"))
+
+# Test "INFO"
+# We are going to use the sequence GET 0
+print ("* Testing INFO...")
+print(c.talk("INFO " + c.talk("GET 0")))
