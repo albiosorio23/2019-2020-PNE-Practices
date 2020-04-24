55ea697e6d6bec2a348a189f270b85d7c341aca7
Albiosorio23 <a.osoriom.2019@alumnos.urj.es>
2020-04-24 14:27:52 +0200

    Develop our own Seq server for working with DNA sequences.

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -1,9 +1,13 @@
 import socket
-
+from Seq1 import Seq
 import termcolor
 
 IP = "127.0.0.1"  # local machines, asi no tienes que ir cambiando el IP
 PORT = 8080
+
+FOLDER = "../Session-04/"
+txt = ".txt"
+
 # Step 1: creating the socket for comunicating
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -21,13 +25,13 @@ print("Server is configured!")
 list_response = ["AGCGTA", "CTATGC", "GGATCG", "TGTAAA", "GGGTT"]
 
 while True:
-
+    print("Waiting for clients...")
     try:
         # Step 4: Wait for client to connect
         (cs, client_ip_port) = ls.accept()
 
     except KeyboardInterrupt:
-        print("Server is done!")
+        print("Server stopped!")
         ls.close()
         exit()
 
@@ -36,25 +40,58 @@ while True:
         # Step 5: Receiving information from the client
         msg_raw = cs.recv(2000)
         msg = msg_raw.decode()
-        # space = msg.split("\n")[0].split(" ")
-        # command1 = space[0]
         response = ""
-        command2 = msg[1]
 
         if msg == "PING":
             response = "OK!\n"
 
-        termcolor.cprint("PING command!", "green")
-        print(response)
-
-        # for i in range(list_response):
         elif "GET" in msg:
+            # Para coger solo el número
             seq_get = int(msg[msg.find(" ") + 1:]) # Seq_get es el número del 0-4 que introduce en la terminal
+            # Devuelve la secuencia en la posición coorespondiente al nº seq_get
             response = list_response[seq_get]
 
         # Info command
         elif "INFO" in msg:
-            
+            gene_seq = msg[msg.find(" ")+1:] #Secuencia introducida en la terminal
+
+            seq = Seq(gene_seq)
+            seq_length = len(gene_seq)
+
+            print("Total length: ", seq_length)
+            number_of_A = seq.count_base("A")
+            percentage_A = "{:.1f}".format(100 * number_of_A / seq_length)
+            number_of_G = seq.count_base("G")
+            percentage_G = "{:.1f}".format(100 * number_of_G / seq_length)
+            number_of_T = seq.count_base("T")
+            percentage_T = "{:.1f}".format(100 * number_of_T / seq_length)
+            number_of_C = seq.count_base("C")
+            percentage_C = "{:.1f}".format(100 * number_of_C / seq_length)
+
+            response = ("A: ", number_of_A, percentage_A, "%" + "\n","C: ", number_of_C, percentage_C, "%" + "\n", "G: ", number_of_G, percentage_G, "%" + "\n", "T: ", number_of_T, percentage_T, "%" + "\n")
+
+        elif "COMP" in msg:
+            gene_seq = msg[msg.find(" ") + 1:]
+            seq = Seq(gene_seq)
+            response = seq.complement() + "\n"
+
+        elif "REV" in msg:
+            gene_seq = msg[msg.find(" ") + 1:]
+            seq = Seq(gene_seq)
+            response = seq.reverse() + "\n"
+
+        elif "GENE" in msg:
+            gene_name = msg[msg.find(" ") + 1:]
+            s = Seq()
+            s.read_fasta(FOLDER + gene_name + txt)
+            response = str(s) + "\n"
+
+        else:
+            termcolor.cprint("Unknown command!!!", 'red')
+            response = "Unkwnown command"
+
+        termcolor.cprint(msg[:msg.find(" ")], "green")
+        print (response)
 
         cs.send(response.encode())
         cs.close()
